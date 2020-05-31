dcc7964d1b690faad7601f57b115d473907c54aa
umohsin001 <64581515+umohsin001@users.noreply.github.com>
2020-05-31 19:54:25 +0600

    sentiment analysis of youtube video comments
    Comments from the final trailer of Marvel's Black Widow movie was analyzed to see the sentiments of the users/viewers. Same was done for a video regarding SpaceX.

---
+++ blackwidow_n_spaceX_sentiment_analysis.R
@@ -0,0 +1,42 @@
+library(tuber)
+library(syuzhet)
+library(ggplot2)
+
+app_id = "xxxxxxxxxxxxxxxxxxxx"
+app_secret = "xxxxxxxxxxxxxxxxx"
+yt_oauth(app_id, app_secret)
+
+## sentiment analysis of black widow final trailer's comment thread
+# getting data from youtube 
+blackwidow = get_comment_threads(filter = c(video_id = "ybji16u608U"))
+
+# cleaning data
+bw = iconv(blackwidow$textDisplay)
+
+# sentiment analysis
+s = get_nrc_sentiment(bw)
+head(s)
+
+# plotting result
+barplot(colSums(s),
+        las = 2,
+        col = rainbow(10),
+        ylab = "count",
+        main = "black widow final trailer sentiment analysis")
+
+# sentiment analysis of comments on video regarding spaceX
+get_stats("Wn5HxXKQOjw")
+com = get_comment_threads(filter = c(video_id = "Wn5HxXKQOjw"))
+
+
+str(com)
+senti = iconv(com$textDisplay)
+
+s1 = get_nrc_sentiment(senti)
+head(s1)
+
+barplot(colSums(s1),
+        las = 2,
+        col = rainbow(10),
+        ylab = "count",
+        main = "sentiment analysis")
